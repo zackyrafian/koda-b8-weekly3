cc85daa31b0ba62b95a7d71a90630bb7c15f0ae5
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-07-07 23:51:42 +0700

    feat: product display

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"seindonesia/internal/domain"
+)
+
+
 func main() { 
-  
+
+  productJson, err := os.Open("../shared/data/product.json")
+  if err != nil { 
+    fmt.Println(err)
+  }
+
+  var product[] domain.Product
+  byteValue, err := io.ReadAll(productJson)
+  err = json.Unmarshal(byteValue, &product)
+
+  for _, p := range product { 
+    fmt.Println(p.ID)
+    fmt.Println(p.Name)
+    fmt.Println(p.Description)
+    fmt.Println(p.Category.DisplayName)
+    fmt.Println(p.Price)
+  }
 }
